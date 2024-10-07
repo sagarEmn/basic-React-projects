205abfb79753720ee18b6f1f0b46f0dcf8e9f384
Sagar <jussagar1@gmail.com>
2024-10-07 17:06:09 +0545

    PasswordGenerator basic functionality working

--- 1 - Password Generator/src/Components/PasswordGenerator.jsx
+++ 1 - Password Generator/src/Components/PasswordGenerator.jsx
@@ -5,89 +5,93 @@ function PasswordGenerator() {
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState();
+
+  // reference to password (useRef)
+  const passwordRef = useRef(null);
+
+  // password generator algorithm
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*(){}[]`~";
+
+    for (let i = 1; i <= length; i++) {
+      let index = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(index);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  const copyPasswordToClipboard = () => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password);
+  };
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed]);
+
+  return (
+    <>
+      {/* main  */}
+      <div className="main">
+        <h1>Password generator</h1>
+
+        {/* Password generator box */}
+        <div className="password__generator-section">
+          {/* password generator input box */}
+          <input
+            ref={passwordRef}
+            type="text"
+            value={password}
+            placeholder="Generating Password"
+          />
+
+          {/* password copy button */}
+          <button onClick={copyPasswordToClipboard}>Copy Password!</button>
+        </div>
+
+        {/* password key parameters */}
+        <div className="password__generator__parameter__section">
+          <input
+            className="password__generator__parameter"
+            type="range"
+            min={6}
+            max={20}
+            value={length}
+            onChange={(fireEvent) => {
+              setLength(fireEvent.target.value);
+            }}
+          />
+          <label>Length: {length} </label>
+
+          <input
+            className="password__generator__parameter"
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
+          <label>Number </label>
+
+          <input
+            className="password__generator__parameter"
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="specialInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
+          />
+          <label>Special Characters </label>
+        </div>
+      </div>
+    </>
+  );
 }
 
-// reference to password (useRef)
-const passwordRef = useRef(null);
-
-// password generator algorithm
-const passwordGenerator = useCallback(() => {
-  let pass = "";
-  let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  if (numberAllowed) str += "0123456789";
-  if (charAllowed) str += "!@#$%^&*(){}[]`~";
-
-  for (let i = 1; i <= length; i++) {
-    let index = Math.floor(Math.random() * str.length + 1);
-    pass += str.charAt(index);
-  }
-
-  setPassword(pass);
-}, [length, numberAllowed, charAllowed, setPassword]);
-
-const copyPasswordToClipboard = () => {
-  passwordRef.current?.select();
-  window.navigator.clipboard.writeText(password);
-};
-
-useEffect(() => {
-  passwordGenerator();
-}, [length, numberAllowed, charAllowed]);
-
-return (
-  <>
-    {/* main  */}
-    <div className="main">
-      <h1>Password generator</h1>
-
-      {/* Password generator box */}
-      <div className="password__generator-section">
-        {/* password generator input box */}
-        <input
-          ref={passwordRef}
-          type="text"
-          value={password}
-          placeholder="Generating Password"
-        />
-
-        {/* password copy button */}
-        <button onClick={copyPasswordToClipboard}>Copy Password!</button>
-      </div>
-
-      {/* password key parameters */}
-      <div className="password__generator__parameter__section">
-        <input
-          type="range"
-          min={6}
-          max={20}
-          value={length}
-          className="password__generator__parameter"
-          onChange={(fireEvent) => {
-            setLength(fireEvent.target.value);
-          }}
-        />
-        <label>Length: </label>
-
-        <input
-          type="checkbox"
-          defaultChecked={numberAllowed}
-          id="numberInput"
-          onChange={() => {
-            setNumberAllowed((prev) => !prev);
-          }}
-        />
-        <label>Number: </label>
-
-        <input
-          type="checkbox"
-          defaultChecked={charAllowed}
-          id="specialInput"
-          onChange={() => {
-            setCharAllowed((prev) => !prev);
-          }}
-        />
-        <label>Special Characters: </label>
-      </div>
-    </div>
-  </>
-);
+export default PasswordGenerator;
