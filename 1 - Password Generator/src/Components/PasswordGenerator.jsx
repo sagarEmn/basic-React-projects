d201eb8bae37f493bea34de75ada22ac9c861f01
Sagar <jussagar1@gmail.com>
2024-10-07 16:56:16 +0545

    Finish basic HTML structure

--- 1 - Password Generator/src/Components/PasswordGenerator.jsx
+++ 1 - Password Generator/src/Components/PasswordGenerator.jsx
@@ -34,3 +34,60 @@ useEffect(() => {
   passwordGenerator();
 }, [length, numberAllowed, charAllowed]);
 
+return (
+  <>
+    {/* main  */}
+    <div className="main">
+      <h1>Password generator</h1>
+
+      {/* Password generator box */}
+      <div className="password__generator-section">
+        {/* password generator input box */}
+        <input
+          ref={passwordRef}
+          type="text"
+          value={password}
+          placeholder="Generating Password"
+        />
+
+        {/* password copy button */}
+        <button onClick={copyPasswordToClipboard}>Copy Password!</button>
+      </div>
+
+      {/* password key parameters */}
+      <div className="password__generator__parameter__section">
+        <input
+          type="range"
+          min={6}
+          max={20}
+          value={length}
+          className="password__generator__parameter"
+          onChange={(fireEvent) => {
+            setLength(fireEvent.target.value);
+          }}
+        />
+        <label>Length: </label>
+
+        <input
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          id="numberInput"
+          onChange={() => {
+            setNumberAllowed((prev) => !prev);
+          }}
+        />
+        <label>Number: </label>
+
+        <input
+          type="checkbox"
+          defaultChecked={charAllowed}
+          id="specialInput"
+          onChange={() => {
+            setCharAllowed((prev) => !prev);
+          }}
+        />
+        <label>Special Characters: </label>
+      </div>
+    </div>
+  </>
+);
