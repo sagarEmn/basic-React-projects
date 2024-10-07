73a149f2a3451b2562c81255016cac645f759495
Sagar <jussagar1@gmail.com>
2024-10-07 17:46:00 +0545

    Better HTML Semantic tags, labels

--- 1 - Password Generator/src/Components/PasswordGenerator.jsx
+++ 1 - Password Generator/src/Components/PasswordGenerator.jsx
@@ -36,60 +36,77 @@ function PasswordGenerator() {
   return (
     <>
       {/* main  */}
-      <div className="main">
-        <h1>Password generator</h1>
+      <main className="password-generator">
+        <h1 className="password-generator__title">Password generator</h1>
 
         {/* Password generator box */}
-        <div className="password__generator-section">
+        <section className="password-generator__output">
           {/* password generator input box */}
           <input
             ref={passwordRef}
             type="text"
             value={password}
             placeholder="Generating Password"
+            className="password-generator__input"
           />
 
           {/* password copy button */}
-          <button onClick={copyPasswordToClipboard}>Copy Password!</button>
-        </div>
+          <button
+            onClick={copyPasswordToClipboard}
+            className="password-generator__copy-btn"
+          >
+            Copy Password!
+          </button>
+        </section>
 
         {/* password key parameters */}
-        <div className="password__generator__parameter__section">
-          <input
-            className="password__generator__parameter"
-            type="range"
-            min={6}
-            max={20}
-            value={length}
-            onChange={(fireEvent) => {
-              setLength(fireEvent.target.value);
-            }}
-          />
-          <label>Length: {length} </label>
+        <section className="password-generator__controls">
+          <div className="password-generator__control">
+            <input
+              className="password-generator__range"
+              type="range"
+              id="length-input"
+              min={6}
+              max={20}
+              value={length}
+              onChange={(fireEvent) => {
+                setLength(fireEvent.target.value);
+              }}
+            />
+            <label htmlFor="length-input" className="password-generator__label">
+              Length: {length}{" "}
+            </label>
+          </div>
 
-          <input
-            className="password__generator__parameter"
-            type="checkbox"
-            defaultChecked={numberAllowed}
-            id="numberInput"
-            onChange={() => {
-              setNumberAllowed((prev) => !prev);
-            }}
-          />
-          <label>Number </label>
+          <div className="password-generator__control">
+            <input
+              id="number-input"
+              className="password-generator__number"
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="number-input" className="password-generator__label">
+              Number{" "}
+            </label>
+          </div>
 
-          <input
-            className="password__generator__parameter"
-            type="checkbox"
-            defaultChecked={charAllowed}
-            id="specialInput"
-            onChange={() => {
-              setCharAllowed((prev) => !prev);
-            }}
-          />
-          <label>Special Characters </label>
-        </div>
-      </div>
+          <div className="password-generator__control">
+            <input
+              id="special-input"
+              className="password__generator__parameter"
+              type="checkbox"
+              defaultChecked={charAllowed}
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="special-input">Special Characters </label>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
