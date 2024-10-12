4acad4ca79113cbae54eac56aec330a61c89589e
Sagar <jussagar1@gmail.com>
2024-10-12 17:59:08 +0545

    Basic functionality todo

--- 2 - Todo App/src/components/Todo.jsx
+++ 2 - Todo App/src/components/Todo.jsx
@@ -1,7 +1,58 @@
-import CheckboxButton from "./CheckboxButton";
-import DeleteButton from "./DeleteButton";
+import React, { useState, useRef, useEffect } from "react";
 
 function Todo() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
+  useEffect(() => {
+    saveTodos();
+  }, [todos]);
+
+  const loadTodos = () => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    setTodos(savedTodos);
+  };
+
+  const saveTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.trim(" ")) {
+      const newTodo = {
+        id: crypto.randomUUID(),
+        text: inputValue.trim(),
+        completed: false,
+      };
+      setTodos([...todos, newTodo]);
+      setInputValue("");
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    e.target.style.height = "auto";
+    e.target.style.height = `${e.target.scrollHeight}px`;
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo;
+      })
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <>
       <main className="todo-app">
@@ -10,15 +61,34 @@ function Todo() {
           <header>
             <h1 className="todo-app__header">Todo React App</h1>
           </header>
+
+          <section className="todo-app__form">
+            <form onSubmit={handleSubmit}>
+              <textarea
+                ref={inputRef}
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder="Write a todo..."
+                rows={1}
+              />
+              <button type="submit">ADD</button>
+            </form>
+          </section>
         </div>
 
         <section className="todo-app__list-section">
           <ul className="todo-app__list">
-            <li className="todo-app__list__item">
-              <CheckboxButton />
-              Do Cleaning
-              <DeleteButton />
-            </li>
+            {todos.map((todo) => {
+              <li key={todo.id} className="todo-app__list__item">
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => toggleTodo(todo.id)}
+                />
+                <span>{todo.text}</span>
+                <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+              </li>;
+            })}
           </ul>
         </section>
       </main>
