77baa7845fe8e294b95c50b2636b4c92ea8e52d0
Sagar <jussagar1@gmail.com>
2024-10-13 10:33:18 +0545

    Better list, icon size

--- 2 - Todo App/public/CheckIcon.jsx
+++ 2 - Todo App/public/CheckIcon.jsx
@@ -4,9 +4,9 @@ export default function CheckIcon({colorFill}) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      height="24px"
+      height="1em"
       viewBox="0 -960 960 960"
-      width="24px"
+      width="1em"
       fill={colorFill}
     >
       <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
