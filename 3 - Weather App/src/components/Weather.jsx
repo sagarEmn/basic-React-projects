ecefc867db8276116f22956c39ef204d48fcb78d
Sagar <desiferchef@gmail.com>
2024-11-17 18:09:19 +0545

    Add units to the api link

--- 3 - Weather App/src/components/Weather.jsx
+++ 3 - Weather App/src/components/Weather.jsx
@@ -17,7 +17,7 @@ function Weather() {
   // fetch api response
   const search = async (city) => {
     try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
         import.meta.env.VITE_APP_ID
       }`;
 
