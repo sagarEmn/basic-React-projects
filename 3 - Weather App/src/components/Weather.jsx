99411000c528a71a3f427d9e71f4f9858112a0f7
Sagar <desiferchef@gmail.com>
2024-11-17 17:58:46 +0545

    API key working

--- 3 - Weather App/src/components/Weather.jsx
+++ 3 - Weather App/src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Weather.css";
 
 // import images
@@ -12,6 +12,23 @@ import wind_icon from "../assets/images/wind.png";
 import humidity_icon from "../assets/images/humidity.png";
 
 function Weather() {
+
+  const search = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`
+
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+
+    }
+  }
+
+  useEffect(() => {
+    search("Banepa")
+  }, [])
+
   return (
     <main className="weather">
       <section className="search-bar">
