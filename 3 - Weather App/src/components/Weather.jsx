df832f766d33e3190ee8fa0f9472d099c6abb5c3
Sagar <desiferchef@gmail.com>
2024-11-17 21:49:00 +0545

    Minor changes

--- 3 - Weather App/src/components/Weather.jsx
+++ 3 - Weather App/src/components/Weather.jsx
@@ -71,15 +71,22 @@ function Weather() {
     search("Banepa");
   }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    search(inputRef.current.value);
+  };
+
   return (
     <main className="weather">
       <section className="search-bar">
-        <input ref={inputRef} type="text" name="city" placeholder="Search" />
-        <img
-          src={search_icon}
-          alt=""
-          onClick={() => search(inputRef.current.value)}
-        />
+        <form onSubmit={handleSubmit} className="search-bar">
+          <input ref={inputRef} type="text" name="city" placeholder="Search" />
+          <img
+            src={search_icon}
+            alt=""
+            onClick={() => search(inputRef.current.value)}
+          />
+        </form>
       </section>
 
       {weatherData ? (
