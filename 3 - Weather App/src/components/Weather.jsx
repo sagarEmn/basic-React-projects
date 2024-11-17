4f35374ed905e515aa10585a06909fd7eadcfc7e
Sagar <desiferchef@gmail.com>
2024-11-17 17:31:56 +0545

    Finish basic UI

--- 3 - Weather App/src/components/Weather.jsx
+++ 3 - Weather App/src/components/Weather.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
-import './Weather.css';
+import React from "react";
+import "./Weather.css";
 
+// import images
+import search_icon from "../assets/images/search.png";
+import clear_icon from "../assets/images/clear.png";
+import cloud_icon from "../assets/images/cloud.png";
+import drizzle_icon from "../assets/images/drizzle.png";
+import rain_icon from "../assets/images/rain.png";
+import snow_icon from "../assets/images/snow.png";
+import wind_icon from "../assets/images/wind.png";
+import humidity_icon from "../assets/images/humidity.png";
 
 function Weather() {
   return (
-    <div>
-        <h1>Weather</h1>
-    </div>
-  )
+    <main className="weather">
+      <section className="search-bar">
+        <input type="text" name="city" placeholder="search" />
+        <img src={search_icon} alt="" />
+      </section>
+      <section className="weather-details">
+        <img src={clear_icon} alt="" className="weather-icon" />
+        <p className="temperature">16 C</p>
+        <p className="location">London</p>
+      </section>
+
+      <section className="weather-data">
+        <div className="col">
+          <img src={humidity_icon} alt="" />
+          <div>
+            <p>91 %</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src={wind_icon} alt="" />
+          <div>
+            <p>3.6 Km/h</p>
+            <span>Wind Speed</span>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default Weather
+export default Weather;
